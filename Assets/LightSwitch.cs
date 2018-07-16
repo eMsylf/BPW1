713f91de63dfea329ce52d45b2840eb6686483e8
Bob Jeltes <bobjeltes@hotmail.com>
2018-07-16 19:27:12 +0200

    Initial commit

---
+++ Assets/LightSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour {
+
+    public Light flashLight;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { flashLight.enabled = !flashLight.enabled; }
+    }
+}
